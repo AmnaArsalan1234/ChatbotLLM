836cfad86c073e1322ae2312c02e5d70174c7aa8
AmnaArsalan1234 <138375903+AmnaArsalan1234@users.noreply.github.com>
2023-07-13 13:48:32 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,31 +1,24 @@
 import streamlit as st
-
 st.title('🎈 ChatBot')
-
 st.write('Hello world!')
-import streamlit as st
 from streamlit_chat import message
 from streamlit_extras.colored_header import colored_header
 from streamlit_extras.add_vertical_space import add_vertical_space
 from hugchat import hugchat
-
 st.set_page_config(page_title="HugChat - An LLM-powered Streamlit app")
-
 # Sidebar contents
 with st.sidebar:
     st.title('🤗💬 HugChat App')
     st.markdown('''
     ## About
-    This app is an LLM-powered chatbot built using:
+This app is an LLM-powered chatbot built using:
     - [Streamlit](https://streamlit.io/)
     - [HugChat](https://github.com/Soulter/hugging-chat-api)
     - [OpenAssistant/oasst-sft-6-llama-30b-xor](https://huggingface.co/OpenAssistant/oasst-sft-6-llama-30b-xor) LLM model
-    
     💡 Note: No API key required!
     ''')
     add_vertical_space(5)
     st.write('Made with ❤️ by [Data Professor](https://youtube.com/dataprofessor)')
-
 # Generate empty lists for generated and past.
 ## generated stores AI generated responses
 if 'generated' not in st.session_state:
@@ -33,7 +26,6 @@ if 'generated' not in st.session_state:
 ## past stores User's questions
 if 'past' not in st.session_state:
     st.session_state['past'] = ['Hi!']
-
 # Layout of input/response containers
 input_container = st.container()
 colored_header(label='', description='', color_name='blue-30')
